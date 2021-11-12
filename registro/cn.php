d20d331ea1048b3c1ea25d85eb5ff1c63acbf0c0
Sistemas computacionales <94130325+sistemas-compu01@users.noreply.github.com>
2021-11-12 08:58:16 -0700

    Delete registro directory

--- registro/cn.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conexion = mysqli_connect("localhost", "root", "", "bd_formulario");
-if (!$conexion)
-{
-    echo 'Error al conectar a la base de datos';
-}
-else {
-    echo 'Conectado a la base de datos';
-}
