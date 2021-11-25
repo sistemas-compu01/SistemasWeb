b4e7f312be9e86323d826987ac50ee0706ef8e2f
Sistemas computacionales <94130325+sistemas-compu01@users.noreply.github.com>
2021-11-25 08:29:35 -0700

    Add files via upload

---
+++ proveedores/pro_registrar.php
@@ -0,0 +1,44 @@
+<?php
+include '../cn.php';
+$nombre = $_POST["nombre"];
+$direccion = $_POST["direccion"];
+$correo = $_POST["correo"];
+$telefono = $_POST["telefono"];
+$RFC = $_POST["RFC"];
+
+$insertar = "INSERT INTO tblproveedores(nombre, direccion, correo, telefono, RFC) VALUES ('$nombre',
+ '$direccion','$correo', '$telefono', '$RFC', )";
+
+
+
+$verificar_correo = mysqli_query($conexion, "SELECT * FROM  tblproveedores WHERE correo = '$correo'");
+if(mysqli_num_rows($verificar_correo) > 0){
+    echo '<script>
+        alert("El correo ya esta registrado");
+        window.history.go(-1);
+    </script>';
+    exit;
+}
+
+$verificar_RFC = mysqli_query($conexion, "SELECT * FROM  tblproveedores WHERE RFC = '$RFC'");
+if(mysqli_num_rows($verificar_usuario) > 0){
+    echo '<script>
+        alert("El RFC ya esta registrado");
+        window.history.go(-1);
+    </script>';
+    exit;
+}
+
+
+$resultado = mysqli_query($conexion, $insertar);
+if(!$resultado){
+    echo 'Error al registrarse';
+} else{
+    echo '<script>
+        alert("El proveedor ha sido registrado exitosamente");
+        window.history.go(-1);
+    </script>';
+}
+
+
+mysqli_close($conexion);
